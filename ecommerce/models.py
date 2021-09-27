69b9a6c8deb3e15f2dbd6146ea697b69fc0d3882
Arturomtz8 <arturo.mtz8@gmail.com>
2021-09-27 12:35:22 -0500

    Creacion de primer modelo para un producto

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+
