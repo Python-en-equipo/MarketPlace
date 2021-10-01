817d9723dd03e56f9f6cde6f5442da07e76c2126
Arturomtz8 <arturo.mtz8@gmail.com>
2021-10-01 14:19:35 -0500

    Modelo modificado

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -3,6 +3,4 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    inventory = models.IntegerField()
-
+    price = models.IntegerField()
