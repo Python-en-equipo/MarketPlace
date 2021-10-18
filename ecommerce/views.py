51ac5b6536117799e35ad6a97a5b7917f2c0b2df
Arturomtz8 <arturo.mtz8@gmail.com>
2021-10-18 10:36:46 -0500

    Añadido redis, postgresql y registrado product en admin para poder editar desde el admin interface

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.views.decorators.cache import cache_page
+print(settings)
+
+queryset = Product.objects.filter(price__gt=50)
+
+print(settings)
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
+
+@cache_page(CACHE_TTL)
 def show_my_page(request):
-    return render(request, 'base.html')
+    return render(request, 'base.html', {'result': queryset})
