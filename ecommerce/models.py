12ae7042aa5bb87b6c23865dd861132d1cbfbfe5
Arturomtz8 <arturo.mtz8@gmail.com>
2021-11-08 14:41:05 -0600

    Redis y forms para crear y editar productos

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.urls import reverse
 
 class Product(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
     price = models.IntegerField()
+
+    # def get_absolute_url(self):
+    #     return reverse("detail", kwargs={"id": self.id})
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
-from .models import Product
-
+from .forms import ProductForm
 from django.conf import settings
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.views.decorators.cache import cache_page
 
-queryset = Product.objects.filter(price__gt=50)
+from .models import Product
 
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 @cache_page(CACHE_TTL)
 def show_my_page(request):
-    return render(request, 'base.html', {'result': queryset})
+    queryset = Product.objects.all()
+    return render(request, 'ecommerce/base.html', {'result': queryset})
+
+def product_create(request):
+    form = ProductForm(request.POST)
+    if form.is_valid():
+        product = form.save()
+        # form = ProductForm()
+        return HttpResponseRedirect(reverse('ecommerce:product-detail', kwargs={'id': product.id}))
+
+    return render(request, 'ecommerce/product_edit.html', {'form': form})
+
+def product_edit_view(request, id):
+    obj = Product.objects.get(id=id)
+    form = ProductForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        form.save()
+        # form = ProductForm()
+        return HttpResponseRedirect(reverse('ecommerce:home'))
+    
+    return render(request, 'ecommerce/product_edit.html', {'form': form})
+
+def product_detail_view(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, 'ecommerce/product_detail.html', {'product': product})
