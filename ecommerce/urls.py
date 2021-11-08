f30b18d7a131b51843b4ce020a33aac4bfb18134
Arturomtz8 <arturo.mtz8@gmail.com>
2021-11-08 14:41:05 -0600

    Redis y forms para crear y editar productos

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
-
+app_name = 'ecommerce'
 urlpatterns = [
-    path('home/', views.show_my_page)
+    path('home/', views.show_my_page, name='home'),
+    path('edit/<int:id>', views.product_edit_view, name='product-edit'),
+    path('new/', views.product_create, name='product-create'),
+    path('detail/<int:id>', views.product_detail_view, name='product-detail')
 ]
