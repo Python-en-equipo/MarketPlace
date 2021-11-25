7929550ab50297de0ec19c8befde7a338ec27ec5
Arturomtz8 <arturo.mtz8@gmail.com>
2021-11-24 21:14:11 -0600

    Se pueden crear productos con imagenes

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 import debug_toolbar
 from django.urls import path, include
 
@@ -7,3 +9,9 @@ urlpatterns = [
     path('ecommerce/', include('ecommerce.urls')),
     path('__debug__/', include(debug_toolbar.urls))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)
