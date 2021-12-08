509be6c660ae24e37e5c1e70a1d6c24c5b768737
Arturomtz8 <arturo.mtz8@gmail.com>
2021-12-07 19:10:42 -0600

    View creada para que user pueda acceder o registrarse

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Product, Image
 
 class ProductForm(forms.ModelForm):
@@ -27,4 +29,14 @@ class ImageForm(forms.ModelForm):
         model = Image
         fields = [
             'image_location'
+        ]
+
+class UserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password1', 
+            'password2'
         ]
