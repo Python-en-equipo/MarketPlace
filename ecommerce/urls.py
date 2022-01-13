fb9e0886e27c79d9c8176da9cd8f1575a61f2342
Arturomtz8 <arturo.mtz8@gmail.com>
2022-01-13 15:25:10 -0600

    añadido pylint

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = "ecommerce"
 urlpatterns = [
     path("home/", views.show_my_page, name="home"),
-    path("edit/<int:id>", views.product_edit_view, name="product-edit"),
+    path("edit/<int:product_id>", views.product_edit_view, name="product-edit"),
     path("new/", views.product_create, name="product-create"),
-    path("detail/<int:id>", views.product_detail_view, name="product-detail"),
-    path("delete/<int:id>", views.product_deletion, name="product-delete"),
+    path("detail/<int:product_id>", views.product_detail_view, name="product-detail"),
+    path("delete/<int:product_id>", views.product_deletion, name="product-delete"),
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
