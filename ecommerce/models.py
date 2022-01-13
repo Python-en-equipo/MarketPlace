59a36dbf8b7809c322592c6905d655983d70cc5a
Arturomtz8 <arturo.mtz8@gmail.com>
2022-01-13 11:36:05 -0600

    corregidos errores de flake

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,7 +1,6 @@
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.db.models.deletion import CASCADE
-from django.urls import reverse
 
 
 class Product(models.Model):
