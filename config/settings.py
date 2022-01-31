2737988854883583b8d84692072d62e30249d33b
Arturomtz8 <arturo.mtz8@gmail.com>
2022-01-31 15:03:04 -0600

    cambiada la lógica del html de home porque mostraba el boton de crear producto aunque fuera un usuario normal

--- config/settings.py
+++ config/settings.py
@@ -103,25 +103,25 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# if os.environ.get("GITHUB_WORKFLOW"):
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.postgresql",
-#             "NAME": "github_actions",
-#             "USER": "postgres",
-#             "PASSWORD": "postgres",
-#             "HOST": "127.0.0.1",
-#             "PORT": "5432",
-#         }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
+# }
+
+
+if os.environ.get("GITHUB_WORKFLOW"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "github_actions",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
+        }
+    }
 
 
 
