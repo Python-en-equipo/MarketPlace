404bb9984f831ee1df355c23ca32eaa95af94fe3
Arturomtz8 <arturo.mtz8@gmail.com>
2022-02-19 16:23:03 -0600

    restaurada base de datos

--- config/settings.py
+++ config/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql_psycopg2",
