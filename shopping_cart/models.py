04175c1bc9b6d6e284309ecf7ab3381b9d82b79d
davidmr27 <davidmoran464@gmail.com>
2022-03-12 22:25:14 -0500

    crear tablas de la base de datos y establecer las relaciones #46

--- shopping_cart/models.py
+++ shopping_cart/models.py
@@ -1,14 +1,29 @@
 from django.db import models
+from ecommerce.models import Product
+from users.models import CustomUser
 
 # Create your models here.
-class Cart(models.Model):
-  pass
 
-class Order(models.Model):
-  pass
+
+class CartItem(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class OrderItem(models.Model):
+    user_id = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
+    total = models.DecimalField(max_digits=10,decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
 class OrderDetails(models.Model):
-  pass
+    product_id = models.ForeignKey(Product, on_delete=models.PROTECT)
+    order_id = models.ForeignKey(OrderItem, on_delete=models.PROTECT)
+    preci = models.DecimalField(max_digits=10,decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-class Payment(models.Model):
-  pass
+
+class PaymentDetails(models.Model):
+    pass
