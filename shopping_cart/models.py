79da6c1380ee63489e77b2aec8defbe50ff0cda9
davidmr27 <davidmoran464@gmail.com>
2022-03-12 21:23:04 -0500

    Modelos y vista iniciales del carrito de compras #46

--- shopping_cart/models.py
+++ shopping_cart/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Cart(models.Model):
+  pass
+
+class Order(models.Model):
+  pass
+
+class OrderDetails(models.Model):
+  pass
+
+class Payment(models.Model):
+  pass
--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .shopping_cart import ShoppingCart
 
 # Create your views here.
+def store_view(request):
+  # Este codigo esta probandose
+  product = request.GET
+  print(product)
+  cart = ShoppingCart()
+  cart.add_product(product)
+  return JsonResponse(cart.cart)
