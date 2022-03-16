969667c6e4032714fadd2bb1caa53a69aeb967c0
Fernando Guerrero <devrrior@gmail.com>
2022-03-15 21:49:14 -0600

    fix(settings.py): set by default databses for test

--- config/settings.py
+++ config/settings.py
@@ -144,7 +144,7 @@ if os.environ.get("GITHUB_WORKFLOW"):
     }
 
 if "test" in sys.argv:
-    DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3','NAME': 'mydatabase',}}
+    DATABASES["default"] = {"ENGINE": "django.db.backends.sqlite3", "NAME": "mydatabase"}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
