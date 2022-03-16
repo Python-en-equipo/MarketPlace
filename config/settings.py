5d43c442cb5b4586af7591fca5d05d5831ad8df8
Fernando Guerrero <devrrior@gmail.com>
2022-03-15 22:32:47 -0600

    fix(project): add django-storages to make image upload work

--- config/settings.py
+++ config/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "storages",
+    "storages",
     "ecommerce",
     "users",
 ]
