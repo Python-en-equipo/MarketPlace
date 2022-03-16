3dca7fcf5e61d733c14c9f41296c5d45ec7a4af0
Fernando Guerrero <devrrior@gmail.com>
2022-03-16 12:41:00 -0600

    feat(project): add payment app's urls in config/urls.py

--- payment/urls.py
+++ payment/urls.py
@@ -1,7 +1,11 @@
 from . import views
-from django.urls import  path
+from django.urls import path
 
+app_name = 'payment'
 
 urlpatterns = [
- 
+    path('checkout_session/', views.create_checkout_session, name='create-checkout-session'),
+    path('test/', views.payment_test, name='test'),
+    path('success/', views.payment_success, name='success'),
+    path('cancel/', views.payment_cancel, name='cancel'),
 ]
