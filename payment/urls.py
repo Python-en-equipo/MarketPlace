fcdff825fa3be0cbfcebb1ba916eb2f8582b741c
Fernando Guerrero <devrrior@gmail.com>
2022-03-16 14:31:00 -0600

    feat(payment): add handle hook view

--- payment/urls.py
+++ payment/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 app_name = 'payment'
 
 urlpatterns = [
-    path('checkout_session/', views.create_checkout_session, name='create-checkout-session'),
+    path('checkout-session/', views.create_checkout_session, name='create-checkout-session'),
+    path('webhooks/stripe/', views.stripe_webhook, name='stripe-webhook'),
     path('test/', views.payment_test, name='test'),
     path('success/', views.payment_success, name='success'),
     path('cancel/', views.payment_cancel, name='cancel'),
--- payment/views.py
+++ payment/views.py
@@ -1,11 +1,23 @@
 import stripe
-from config.settings import STRIPE_PRIVATE_KEY
+from config.settings import STRIPE_PRIVATE_KEY, STRIPE_WEBHOOK_KEY
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 YOUR_DOMAIN = 'http://127.0.0.1:8000'
 
 stripe.api_key = STRIPE_PRIVATE_KEY
+
+def payment_test(request):
+    return render(request, 'payment/payment.html')
+
+def payment_success(request):
+    return render(request, 'payment/success.html')
+
+def payment_cancel(request):
+    return render(request, 'payment/cancel.html')
+
 def create_checkout_session(request):
     if request.method == 'POST':
         try:
@@ -13,7 +25,7 @@ def create_checkout_session(request):
                 line_items=[
                     {
                         'price_data': {
-                            'currency': 'usd', # moneda de pago
+                            'currency': 'mxn', # moneda de pago
                             'product_data':{ # informacion del producto, tambien se puede agregar descripcion, pero no veo necesario
                                 'name': 'microfono',
                             },
@@ -22,6 +34,9 @@ def create_checkout_session(request):
                         'quantity': 2, # cantidad de productos que se comprara
                     }
                 ],
+                # customer_email=self.request.user.email, # for pass user email
+                payment_method_types = ['card','oxxo'],
+                billing_address_collection='required', # pedir direccion
                 mode='payment',
                 success_url=request.build_absolute_uri(reverse_lazy('payment:success')),
                 cancel_url=request.build_absolute_uri(reverse_lazy('payment:cancel')),
@@ -32,12 +47,40 @@ def create_checkout_session(request):
 
         return redirect(checkout_session.url)
 
-def payment_test(request):
-    return render(request, 'payment/payment.html')
 
-def payment_success(request):
-    return render(request, 'payment/success.html')
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
 
-def payment_cancel(request):
-    return render(request, 'payment/cancel.html')
+    try:
+        event = stripe.Webhook.construct_event(
+          payload, sig_header, STRIPE_WEBHOOK_KEY
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        # TODO Obtener datos del cliente
+        session = event['data']['object']
+
+        # line_items = event['line_items']
+        # customer_name = session['shipping']['name']
+        print('event', event)
+        customer_email = session['customer_details']['email']
+        customer_shipping = session['shipping']
+        # print(session)
+        # print('customer_name', customer_name)
+        # print('customer_email', customer_email)
+        # print('customer_shipping', customer_shipping)
+        # print('line_items', line_items)
+
+    # Passed signature verification
+    return HttpResponse(status=200)
 
