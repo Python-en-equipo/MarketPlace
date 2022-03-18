891dfcb29f613fc96f35696f4995eed138d74a49
davidmr27 <davidmoran464@gmail.com>
2022-03-17 23:19:10 -0500

    endpoint de agregar el producto al carrito #46

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import store_view
+from .views import list_products_cart, add_product_cart
 
 app_name = 'shopping_cart'
 
 urlpatterns = [
-  path("add_product/",store_view, name="add_product")
+  path("", list_products_cart, name="home"),
+  path("add_product/<int:product_id>",add_product_cart, name="add_product"),
+  # path("remove_product/",store_view, name="delete_product"),
+  # path("update_product/",store_view, name="update_product"),
 ]
