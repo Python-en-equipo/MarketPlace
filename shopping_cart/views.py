6676e459950387b504eede86ae51c1145984a2a5
davidmr27 <davidmoran464@gmail.com>
2022-03-19 22:48:31 -0500

    vista para listar, eliminar, quitar los productos del carrito #46

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
@@ -42,10 +42,45 @@ def add_product_cart(request, product_id):
         cart_item.save()
 
     # base_url = reverse('ecommerce')
-    return redirect("ecommerce:home")
+    return redirect("shopping_cart:home")
 
-
+@login_required
 def list_products_cart(request):
-    products = Product.objects.all()
-    images = Image.objects.all()
-    return render(request, 'cart/cart.html', {"products": products, "images": images})
+    cart_items = None
+    total = 0
+    quantity = 0
+    try:
+        cart = CartSession.objects.get(session_id=_get_session_id(request))
+        cart_items = CartItem.objects.filter(cart=cart,is_active=True)
+        for item in cart_items:
+            total += item.get_subtotal()
+
+        print(f"Total: {total}")
+    except CartItem.DoesNotExist:
+        pass
+    except CartItem.DoesNotExist:
+        pass
+
+    return render(request, 'cart/cart.html', {"cart_items": cart_items, "total": total})
+
+
+@login_required
+def delete_product_cart(request, product_id):
+    cart = CartSession.objects.get(session_id=_get_session_id(request))
+    product = Product.objects.get(id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('shopping_cart:home')
+
+@login_required
+def remove_product_cart(request, product_id):
+    cart = CartSession.objects.get(session_id=_get_session_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    item = CartItem.objects.get(product=product,cart=cart)
+    if item.quantity > 1:
+        item.quantity -= 1
+        item.save()
+    else:
+        item.delete()
+
+    return redirect('shopping_cart:home')
