6354acfd54b0eaf5fc25afa9563846b821ade4b0
Fernando Guerrero <devrrior@gmail.com>
2022-03-24 18:57:49 -0600

    test deploy con nuevos cambios en heroku de prueba

--- config/settings.py
+++ config/settings.py
@@ -233,8 +233,8 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = env("EMAIL")
-EMAIL_HOST_PASSWORD = env('GMAIL_TOKEN')
+# EMAIL_HOST_USER = env("EMAIL")
+# EMAIL_HOST_PASSWORD = env('GMAIL_TOKEN')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
