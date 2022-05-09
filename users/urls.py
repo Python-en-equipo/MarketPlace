fd172b25df799ce4a4472e25bed247c298b3bb98
Fernando Guerrero <devrrior@gmail.com>
2022-05-08 21:09:24 -0500

    feat(users/urls): create new endpoint for create new user

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,7 @@ from . import views
 # no borrar porque los templates usan la appname
 app_name = "users"
 
-urlpatterns = [path("user/<int:pk>", views.user_detail, name="user_detail")]
+urlpatterns = [
+    path("user/<int:pk>", views.user_detail, name="user_detail"),
+    path("signup/", views.user_create, name="user_create"),
+]
