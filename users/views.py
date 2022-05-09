1827de74bb249f5c41583da3e560ef558e7cadc1
Fernando Guerrero <devrrior@gmail.com>
2022-05-08 21:09:05 -0500

    feat(users/views): create view for create new user

--- users/views.py
+++ users/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from users.models import CustomUser
 from users.serializers import UserSerializer
+
+
+@api_view(['POST'])
+def user_create(request):
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            print(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
