b75962b5db6a5fda4629c1823aad74a6e10de19c
Fernando Guerrero <devrrior@gmail.com>
2022-05-08 19:17:45 -0500

    feat(users/urls): add view for user_detail

--- users/urls.py
+++ users/urls.py
@@ -5,11 +5,4 @@ from . import views
 # no borrar porque los templates usan la appname
 app_name = "users"
 
-urlpatterns = [
-    path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
-    path("seller_register/", views.seller_register, name="seller_register"),
-    path("user_panel/", views.user_panel, name="user_panel"),
-    path("user_edit/", views.user_modify_view, name="user_edit"),
-]
+urlpatterns = [path("user/<int:pk>", views.user_detail, name="user_detail")]
