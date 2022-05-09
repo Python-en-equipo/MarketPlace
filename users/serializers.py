d18f615ef4211aab4674f6f281f7f8dabd814050
Fernando Guerrero <devrrior@gmail.com>
2022-05-08 21:08:38 -0500

    feat(users/serializers): add create method for create new user

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,40 @@
+from wsgiref.validate import validator
+
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from users.models import CustomUser
 
 
 class UserSerializer(serializers.ModelSerializer):
+
+    email = serializers.EmailField(
+        required=True, validators=[UniqueValidator(queryset=CustomUser.objects.all())]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = CustomUser
-        fields = "__all__"
+        fields = ('email', "first_name", "last_name", "password", "password2")
+        extra_kwargs = {
+            'email': {'required': True},
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'password': {'required': True, 'write_only': True},
+        }
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({'password': 'Las contraseñas no coinciden'})
+
+        return data
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+
+        user.set_password(validated_data['password'])
+        return user
