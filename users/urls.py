842193ee248bd4c23d2009070b0ee5700c83f3fa
Fernando Guerrero <devrrior@gmail.com>
2022-05-10 21:26:26 -0500

    feat(users/urls): add urls for authentication system

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import views
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path("user/<int:pk>", views.user_detail, name="user-detail"),
     path("signup/", views.user_create, name="user-create"),
     path("seller/<int:pk>", views.seller_detail, name="seller-detail"),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
