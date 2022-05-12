acfdb1de2049fbcb2c7a5a41eac7fc5aa0f8c18c
Fernando Guerrero <devrrior@gmail.com>
2022-05-12 14:08:37 -0500

    fix(users/urls): update name of views for login and login/refresh

--- users/urls.py
+++ users/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("user/<int:pk>", views.user_detail, name="user-detail"),
     path("signup/", views.user_create, name="user-create"),
     path("seller/<int:pk>", views.seller_detail, name="seller-detail"),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(), name="login"),
+    path('login/refresh', TokenRefreshView.as_view(), name="token_refresh"),
 ]
