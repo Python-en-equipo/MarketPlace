f864746560e8ce3e669f129c3c7ba3a77391c375
Arturomtz8 <arturo.mtz8@gmail.com>
2022-05-18 22:01:34 -0500

    ya se hace slug usando el title en serializer

--- ecommerce/serializers.py
+++ ecommerce/serializers.py
@@ -1,6 +1,7 @@
 from os import read
 from ecommerce.models import Category, Product, Image
 from rest_framework import serializers
+from django.utils.text import slugify
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -13,12 +14,17 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     category = serializers.StringRelatedField()
+    title_slug = serializers.SerializerMethodField()
+
+    def get_title_slug(self, instance):
+        return slugify(instance.title)
+
     class Meta:
         model = Product
         # TODO AÑADIR SELLER SERIALIZER
         fields = [
-            "title", "description",
-            "price", "category", "slug",
+            "title_slug", "title", "description",
+            "price", "category",
             "stock"
         ]
 
