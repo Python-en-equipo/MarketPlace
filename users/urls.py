a0bd08876c509bf1eaab234820f9a77b76ce2ebf
Fernando Guerrero <devrrior@gmail.com>
2022-06-23 18:26:34 -0500

    feat(users/urls): update urls

--- users/urls.py
+++ users/urls.py
@@ -6,9 +6,9 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path("<int:pk>", views.user_detail, name="user-detail"),
-    path("", views.user_create, name="user-create"),
-    path('token/', TokenObtainPairView.as_view(), name="login"),
-    path('token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
+    path("<int:pk>", views.UserRetrieveUpdateDestroyAPIView.as_view(), name="user-detail"),
+    path("", views.UserCreateAPIView.as_view(), name="user-create"),
+    path("token/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("seller/<int:pk>", views.seller_detail, name="seller-detail"),
 ]
