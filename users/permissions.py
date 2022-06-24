bb025e2284888f6f132f0d405c2b2b6890118ced
Fernando Guerrero <devrrior@gmail.com>
2022-06-23 22:16:38 -0500

    feat(users): add permissions for seller

--- users/permissions.py
+++ users/permissions.py
@@ -1,7 +1,9 @@
+from email import message
+
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class IsOwnerPermission(BasePermission):
+class IsOwnerUserPermission(BasePermission):
     message = "You do not have permission to manipulate user given"
 
     def has_permission(self, request, view):
@@ -14,3 +16,14 @@ class IsOwnerPermission(BasePermission):
             return True
 
         return obj == request.user
+
+
+class IsOwnerSellerPermission(BasePermission):
+    message = "You do not have permission to do this"
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.seller == request.user
