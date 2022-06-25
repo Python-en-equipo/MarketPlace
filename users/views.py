b6611d83b0f52ce0d68c2d3ef6ff6e839e47a56a
Fernando Guerrero <devrrior@gmail.com>
2022-06-25 09:47:05 -0500

    fix(users/views): add update function

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 
@@ -14,6 +16,16 @@ class UserRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = UserSerializer
     queryset = CustomUser.objects.all()
     permission_classes = [IsOwnerUserPermission, IsAuthenticated]
+
+    def update(self, request, pk):
+        user = self.get_object()
+
+        serializer = self.get_serializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
