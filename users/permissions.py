0b47d1640b445e7678d6428c025a78e78e15abf5
Fernando Guerrero <devrrior@gmail.com>
2022-06-25 09:42:10 -0500

    style(users/permissions): remove def has_permission

--- users/permissions.py
+++ users/permissions.py
@@ -4,10 +4,6 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 class IsOwnerUserPermission(BasePermission):
     message = "You do not have permission to manipulate user given"
 
-    def has_permission(self, request, view):
-
-        return super().has_permission(request, view)
-
     def has_object_permission(self, request, view, obj):
 
         if request.method in SAFE_METHODS:
