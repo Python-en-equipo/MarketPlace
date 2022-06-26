0e6400753adc7cbeeae1047924767c5f1ac8977c
Fernando Guerrero <devrrior@gmail.com>
2022-06-26 10:29:58 -0500

    fix(users): create new serializer for create user and update urls

--- users/urls.py
+++ users/urls.py
@@ -6,12 +6,12 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path("<int:pk>", views.UserRetrieveUpdateDestroyAPIView.as_view(), name="user-detail"),
-    path("", views.UserCreateAPIView.as_view(), name="user-create"),
-    path("token/", TokenObtainPairView.as_view(), name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("users/<int:pk>", views.UserRetrieveUpdateDestroyAPIView.as_view(), name="user-detail"),
+    path("users/", views.UserCreateAPIView.as_view(), name="user-create"),
+    path("users/token/", TokenObtainPairView.as_view(), name="login"),
+    path("users/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path(
-        "seller/<int:pk>", views.SellerRetirieveUpdateDestroyAPIView.as_view(), name="seller-detail"
+        "sellers/<int:pk>", views.SellerRetirieveUpdateDestroyAPIView.as_view(), name="seller-detail"
     ),
-    path("seller/", views.SellerCreateAPIView.as_view(), name="seller-create"),
+    path("sellers/", views.SellerCreateAPIView.as_view(), name="seller-create"),
 ]
